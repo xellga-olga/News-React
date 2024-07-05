3b3b2fcacffa8ff6efef39505331c668b73a91f7
Noskina Olha <noskinaolga1@gmail.com>
2024-07-05 14:20:58 +0200

    news list

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,11 @@ import styles from './styles.module.css'
 const Header = () => {
    return (
       <header className={styles.header}>
-         <h1 className={styles.title}>News React</h1>
+         <h1 className={styles.title}>
+            <span className={styles.wave}>Wave</span>
+            <span className={styles.news}>News</span>
+            <span className={styles.react}>React</span>
+         </h1>
          <p className={styles.date}>{formatDate(new Date())}</p>
       </header>
    )
--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,9 +1,9 @@
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 const Image = ({ image }) => {
   return (
     <div className={styles.wrapper}>
-      {image ? <image src={image} alt='news' className={styles.image} />  : null}
+      {image ? <img src={image} alt="news" className={styles.image} /> : null}
     </div>
   );
 };
