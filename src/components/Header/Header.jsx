5db545d6967b4f02f756a582a9de1b1eca9798ee
Noskina Olha <noskinaolga1@gmail.com>
2024-07-05 15:12:20 +0200

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,22 @@
-import { formatDate } from "../../helpers/formatDate"
-import styles from './styles.module.css'
+import { formatDate } from "../../helpers/formatDate";
+import styles from './styles.module.css';
 
 const Header = () => {
    return (
       <header className={styles.header}>
-         <h1 className={styles.title}>
-            <span className={styles.wave}>Wave</span>
-            <span className={styles.news}>News</span>
-            <span className={styles.react}>React</span>
-         </h1>
-         <p className={styles.date}>{formatDate(new Date())}</p>
+         <div className={styles.titleContainer}>
+            <h1 className={styles.title}>
+               <span className={styles.wave}>Wave</span>
+               <span className={styles.news}>News</span>
+               <span className={styles.react}>React</span>
+            </h1>
+            <p className={styles.date}>{formatDate(new Date())}</p>
+         </div>
+         <p className={styles.credit}>
+            Created by <a href="https://github.com/xellga-olga/News-React" target="_blank" rel="noopener noreferrer" className={styles.link}>Olya Plakhotnikova</a>
+         </p>
       </header>
    )
 }
 
-export default Header
+export default Header;
