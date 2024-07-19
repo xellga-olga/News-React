ea4615a60acbaec63b97cf8dee3bc80181f5912b
Noskina Olha <noskinaolga1@gmail.com>
2024-07-19 12:31:33 +0200

    add latest news

--- src/components/NewsByFilters/NewsByFilters.jsx
+++ src/components/NewsByFilters/NewsByFilters.jsx
@@ -1,10 +1,27 @@
-import { TOTAL_PAGES } from "../../constants/constants";
+import { getNews } from "../../api/apiNews";
+import { PAGE_SIZE, TOTAL_PAGES } from "../../constants/constants";
+import { useDebounce } from "../../helpers/hooks/useDebounce";
+import { useFetch } from "../../helpers/hooks/useFetch";
+import { useFilters } from "../../helpers/hooks/useFilters";
 import NewsFilters from "../NewsFilters/NewsFilters";
 import NewsList from "../NewsList/NewsList";
-import Pagination from "../Pagination/Pagination";
+import PaginationWrapper from "../PaginationWrapper/PaginationWrapper";
 import styles from "./styles.module.css";
 
-const NewsByFilters = ({ filters, changeFilter, isLoading, news }) => {
+const NewsByFilters = () => {
+  const { filters, changeFilter } = useFilters({
+    page_number: 1,
+    page_size: PAGE_SIZE,
+    category: null,
+    keywords: "",
+  });
+
+  const debouncedKeywords = useDebounce(filters.keywords, 1500);
+
+  const { data, isLoading } = useFetch(getNews, {
+    ...filters,
+    keywords: debouncedKeywords,
+  });
 
   const handleNextPage = () => {
     if (filters.page_number < TOTAL_PAGES) {
@@ -24,18 +41,19 @@ const NewsByFilters = ({ filters, changeFilter, isLoading, news }) => {
 
   return (
     <section className={styles.section}>
+      <NewsFilters filters={filters} changeFilter={changeFilter} />
 
-      <NewsFilters filters={filters} changeFilter={changeFilter}/>
-
-      <Pagination
+      <PaginationWrapper
+        top
+        bottom
         handlePrevPage={handlePrevPage}
         handleNextPage={handleNextPage}
         handlePageClick={handlePageClick}
         totalPages={TOTAL_PAGES}
         currentPage={filters.page_number}
-      />
-
-      <NewsList isLoading={isLoading} news={news} />
+      >
+        <NewsList isLoading={isLoading} news={data?.news} />
+      </PaginationWrapper>
     </section>
   );
 };
